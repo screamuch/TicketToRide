5369a548a8df0fc4faf3f824cd26311c8921891d
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 21:55:03 -0700

    Dest tx

--- src/model/Game.java
+++ src/model/Game.java
@@ -3,6 +3,21 @@ package model;
 import java.util.ArrayList;
 
 public class Game {
+    public static void main(String[] args) {
+       // DestinationTicket obj = new DestinationTicket("San Francisco", "Las Vegas", 0);
+        
+       
+        //String c1[]; 
+        //c1 = new String [7];
+        DestinationTicket obj[];
+        obj[] = new DestinationTicket [7];
+        
+         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calagary","Helena" }; 
+         String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" };
+         int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
+         for (int i = 0; i < c1.length; i++)
+           //  DestinationTicket[] obj = new DestinationTicket("San Francisco", "Las Vegas", 0);
+    }
 
 	public static void main(String[] args) {
 		
