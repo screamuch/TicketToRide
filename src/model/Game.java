ea685cf8454e1ab47250858d2ef1ad725391baaa
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 22:29:06 -0700

    Update src/model/Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -18,7 +18,7 @@ public class Game {
          String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; // end destination 
          int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; // value of completed destination 
          for (int i = 0; i < c1.length; i++) // 
-             destinationTicketDeck.add(new DestinationTicket(c1[i],c2[i],pts[i])); 
+             destinationTicketDeck.add(new DestinationTicket(c1[i],c2[i],pts[i])); // hardcoding each destination card into queue
     }
 
 	public static void main(String[] args) {
