966cdfbc76a2afb6d998037ba13f6d17aeec633e
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 22:49:21 -0700

    update styling
    add toString

--- src/model/Game.java
+++ src/model/Game.java
@@ -19,7 +19,7 @@ public class Game {
         pts = value of completed destination 
         destinationTickerDeck = hardcoding each destination card into queue
         */
-         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calagary","Helena" }; 
+         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calgary", "Helena" }; 
          String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; 
          int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
          for ( int i = 0; i < c1.length; i++ ) 
