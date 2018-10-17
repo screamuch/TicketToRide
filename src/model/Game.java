fceb560f4c82f59180dba59cd85cad8984f5c084
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 22:39:50 -0700

    Update src/model/Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -13,12 +13,18 @@ public class Game {
         Queue<DestinationTicket> destinationTicketDeck = new LinkedList<>(); 
       
         
-        
-         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calagary","Helena" }; // hardcoiding start destination
-         String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; // end destination 
-         int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; // value of completed destination 
-         for (int i = 0; i < c1.length; i++) // 
-             destinationTicketDeck.add(new DestinationTicket(c1[i],c2[i],pts[i])); // hardcoding each destination card into queue
+        /*
+        c1 = hardcoiding start destination
+        c2 = end destination 
+        pts = value of completed destination 
+        destinationTickerDeck = hardcoding each destination card into queue
+        */
+         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calagary","Helena" }; 
+         String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; 
+         int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
+         for ( int i = 0; i < c1.length; i++ ) 
+             destinationTicketDeck.add(new DestinationTicket( c1[i], c2[i], pts[i] ));
+         
     }
 
 	public static void main(String[] args) {
