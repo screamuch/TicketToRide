6f885f20da6090f41334974cd03c2caa601f5dbf
damirgafic <44035881+damirgafic@users.noreply.github.com>
2018-10-17 00:18:11 -0700

    Add destination tx (#1)
    * Dest tx
    
    * dest tick
    
    Updated with the queue
    
    * Update src/model/Game.java
    
    * Update src/model/Game.java
    
    * update styling
    
    add toString
    
    * updated toString
    
    updated getter/setter
    
    * toString
    
    * DestinationTicket Update
    
    Added queue method to DestinationTicket and removed from game.java
    
    * Add routes
    
    * add Route desc
    
    * add toString()
    
    * add routes init
    
    * finish routes
    
    * oops
    
    * Dest tx
    
    * dest tick
    
    Updated with the queue
    
    * Update src/model/Game.java
    
    * Update src/model/Game.java
    
    * update styling
    
    add toString
    
    * updated toString
    
    updated getter/setter
    
    * toString
    
    * DestinationTicket Update
    
    Added queue method to DestinationTicket and removed from game.java
    
    * random
    
    * rebase fix
    
    * Merge branch 'add_destination_tx' of https://github.com/PeterChudinov/TicketToRide into add_destination_tx

--- src/model/DestinationTicket.java
+++ src/model/DestinationTicket.java
@@ -5,6 +5,9 @@
  */
 package model;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author damir
@@ -14,16 +17,66 @@ public class DestinationTicket {
     public String city2; 
     public int points; 
     
+    public DestinationTicket()
+    {
+        city1 = "" ;
+        city2 = "" ;
+        points = 0; 
+    }
     public DestinationTicket(String c1, String c2, int pts)
     {
         city1 = c1; 
         city2 = c2; 
         points = pts; 
     }
+
+    
     void setDestCard(String c1, String c2, int pts)
     {
-        city1 = c1; 
-        city2 = c2; 
-        points = pts; 
+        this.city1 = c1; 
+        this.city2 = c2; 
+        this.points = pts; 
     }
+    String getCity1()
+    {
+        return this.city1; 
+    }
+    String getCity2()
+    {
+        return this.city2; 
+    }
+    int getPoints()
+    {
+        return this.points; 
+    }
+    @Override
+    public String toString()
+    {
+        // returns destination card
+        String output = city1 + " -> " + city2 + ", Points: " + points; 
+        return output; 
+    }
+
+    public static Queue<DestinationTicket> initADeck()
+    {
+        /*
+        c1 = hardcoiding start destination
+        c2 = end destination 
+        pts = value of completed destination 
+        destinationTickerDeck = hardcoding each destination card into queue
+        */
+        Queue<DestinationTicket> destinationTicketDeck = new LinkedList<>();
+        String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calgary", "Helena" }; 
+        String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; 
+        int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
+
+         for ( int i = 0; i < c1.length; i++ ) 
+             destinationTicketDeck.add(new DestinationTicket( c1[i], c2[i], pts[i] ));
+         return destinationTicketDeck; 
+    }
+    
 }
+
+
+
+
--- src/model/Game.java
+++ src/model/Game.java
@@ -1,10 +1,12 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Game {
-
 	public static void main(String[] args) {
+            DestinationTicket obj = new DestinationTicket(); 
+            System.out.println(obj.initADeck().element());
 		
 	}
 }
