807148d43d2be29476f923460cb69b5c57f5d9ee
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 23:53:11 -0700

    rebase fix

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,39 +1,6 @@
 package model;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Game {
-    public static void main(String[] args) {
-       // DestinationTicket obj = new DestinationTicket("San Francisco", "Las Vegas", 0);
-        
-       
-        //String c1[]; 
-        //c1 = new String [7];
-       /* Queue<DestinationTicket> destinationTicketDeck = new LinkedList<>(); 
-      
-        
-        
-        c1 = hardcoiding start destination
-        c2 = end destination 
-        pts = value of completed destination 
-        destinationTickerDeck = hardcoding each destination card into queue
-        
-         String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calgary", "Helena" }; 
-         String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; 
-         int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
-         for ( int i = 0; i < c1.length; i++ ) 
-             destinationTicketDeck.add(new DestinationTicket( c1[i], c2[i], pts[i] ));
-         */
-       DestinationTicket obj = new DestinationTicket();
-        
-       // testing function
-       System.out.println(obj.initADeck().element());
-       System.out.println(obj.getCity1());
-       
-       
-       
-    }
 
 	public static void main(String[] args) {
 		
