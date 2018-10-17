b6d03c8ed445679069a9b7051da918db3009d459
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-16 23:13:39 -0700

    DestinationTicket Update
    Added queue method to DestinationTicket and removed from game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -10,21 +10,21 @@ public class Game {
        
         //String c1[]; 
         //c1 = new String [7];
-        Queue<DestinationTicket> destinationTicketDeck = new LinkedList<>(); 
+       /* Queue<DestinationTicket> destinationTicketDeck = new LinkedList<>(); 
       
         
-        /*
+        
         c1 = hardcoiding start destination
         c2 = end destination 
         pts = value of completed destination 
         destinationTickerDeck = hardcoding each destination card into queue
-        */
+        
          String[] c1 = new String[]{ "Portland", "Vancouver", "Denver", "Seattle", "Calgary", "Calgary", "Helena" }; 
          String[] c2 = new String[]{ "Phoenix", "Santa Fe", "El Paso", "Los Angeles", "Salt Lake City", "Phoenix", "Los Angeles" }; 
          int[] pts = new int[]{ 11, 13, 4, 9, 7, 13, 8}; 
          for ( int i = 0; i < c1.length; i++ ) 
              destinationTicketDeck.add(new DestinationTicket( c1[i], c2[i], pts[i] ));
-         
+         */
     }
 
 }
