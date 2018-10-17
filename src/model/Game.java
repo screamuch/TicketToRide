ae6c4878ebb5087a05b1cc0d14a5414a8fc5779a
Adeeb Rahman <arahman8@horizon.csueastbay.edu>
2018-10-17 00:19:22 -0700

    add player (#3)

--- src/model/Game.java
+++ src/model/Game.java
@@ -5,8 +5,6 @@ import java.util.Queue;
 
 public class Game {
 	public static void main(String[] args) {
-            DestinationTicket obj = new DestinationTicket(); 
-            System.out.println(obj.initADeck().element());
-		
+	
 	}
 }
