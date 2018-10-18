96012ab5bb0c70dd5c4df166bc05d02d00409763
peterchudinov <peterchudinov@icloud.com>
2018-10-17 22:44:48 -0700

    add fixes

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,25 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-	
+	private static final int STARTING_TRAINS = 20;
+
+	// not used yet
+	enum Color {
+		RED, BLUE, GREEN, YELLOW, BLACK
+	}
 
 	private String name, color;
 	private int trainCount, score;
 	List<DestinationTicket> destinationTickets;
 
+	// this constructor is only for tests
 	public Player(String name, String color, int trainCount, int score) {
 		this.name = name;
 		this.color = color;
 		this.trainCount = trainCount;
 		this.score = score;
 	}
-	
+
+	// use this constructor for the actual game
+	public Player(String name, String color) {
+		this.name = name;
+		this.color = color;
+		this.trainCount = STARTING_TRAINS;
+		this.score = 0;
+	}
+
 	public String getName() {
 		return name;
 	}
 
 	public String getColor() {
 		return color;
+	}
+
+	public int getTrainCount() {
+		return trainCount;
 	}
 
 	public void reduceTrainCount() {
