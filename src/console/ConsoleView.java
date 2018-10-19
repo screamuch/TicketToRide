56c850378427bed78d3bb7e361eecf97b1989d5f
damirg <44035881+damirgafic@users.noreply.github.com>
2018-10-18 23:20:27 -0700

    consoleView

--- src/console/ConsoleView.java
+++ src/console/ConsoleView.java
@@ -1,5 +1,39 @@
 package console;
 
-public class ConsoleView {
+import java.util.Scanner;
+import model.Player;
 
+public class ConsoleView {
+    private static Scanner stdin = new Scanner(System.in);
+    public void printString()
+    {
+        System.out.println( "Welcome to Ticket To Ride\nPlease enter the amount of players(1-4)" );
+    }
+    public void announceWinner(Player winningPlayer)
+    {
+        System.out.println(winningPlayer.getName());
+    }
+    public void printAskPlayerColor()
+    {
+        System.out.println("What is your color preference?");
+    }
+    public String readPlayerColor(String color)
+    {
+        color = stdin.next();
+        return color;
+    }
+    public void printAskNumPlayers()
+    {
+        System.out.println("How many players are playing the game? (1-4) ");
+    }
+    public int numberOfPlayers(int numPlayers)
+    {
+        numPlayers = stdin.nextInt(); 
+        return numPlayers; 
+    }
+    public void createNewGame()
+    {
+        
+    }
+    
 }
